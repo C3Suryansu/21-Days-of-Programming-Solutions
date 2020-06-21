485ba12adbbfc98f03f2cdedb272b0ba1fe7f927
C3Suryansu <suryansudash5@gmail.com>
2020-06-21 16:08:49 +0530

    Add files via upload

---
+++ day_7/src/main.rs
@@ -0,0 +1,35 @@
+/*
+DAY 7:
+Write a program to check if a given string is palindrome or not.
+
+Details:
+The program can be used to reverse a string or verify if it is a palindrome or not.
+
+Inputs:
+Input any string.
+
+Outputs:
+Print if the string is palindrome or not.
+
+Sample:
+
+Inputs
+ABCDCBA
+
+Outputs:
+Yes
+
+*/
+
+#[macro_use]
+extern crate text_io;
+
+fn main() {
+    let foo:String = read!();
+    let new:&str = &foo;
+    if new == new.chars().rev().collect::<String>(){
+        println!("Palindrome");
+    }else{
+        println!("Not a palindrome");
+    }
+}
