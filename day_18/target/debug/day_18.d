1ff42b2c60c75a7c2f6873fd964fdf41c1bea950
C3Suryansu <suryansudash5@gmail.com>
2020-07-02 19:17:15 +0530

    Added 18th day solution

---
+++ day_18/target/debug/day_18.d
@@ -0,0 +1,1 @@
+E:\Rust\day_18\target\debug\day_18.exe: E:\Rust\day_18\src\main.rs
---
+++ day_18/target/debug/deps/day_18.d
@@ -0,0 +1,5 @@
+E:\Rust\day_18\target\debug\deps\day_18.exe: src\main.rs
+
+E:\Rust\day_18\target\debug\deps\day_18.d: src\main.rs
+
+src\main.rs:
